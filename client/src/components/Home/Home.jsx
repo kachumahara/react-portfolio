e8bbcdbabe37fae9141e34df98d8df9b7d1bca31
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-23 00:06:40 -0700

    added pic to portfolio jsx and also added more pic to image folder

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import Container from "../Container/Container.jsx";
 import ImageChangeOnMouseOver from "./imageEffects/ImageChangeOnMouseOver.jsx";
 import Row from "../Row/Row.jsx";
 import "./Home.css";
-import Banner from './Banner.jsx'
+import Banner from "./Banner.jsx";
 const Home = () => {
   return (
     <Container fluid>
@@ -19,17 +19,30 @@ const Home = () => {
         </div>
         <div className="col-md-5 justify-content center shadow-lg p-3 mb-5 pt-5 rounded">
           <div className="card-body">
-          <h2 className="texty">About</h2>
-          <br />
+            <h2 className="texty">About</h2>
+            <br />
             <p className="card-text font-weight-bold" id="bio">
-            My name is Kenneth Achumahara, a Software Developer. I earned my first degree in Accounting and most recently obtained a Certificate in Full Stack Web Development from the University of Arizona. With a disciplined mindset, I love taking difficult tasks and rendering valued solutions that are exceptional with unique qualities. Enthusiastic about approaching programming challenges from different angles and collaborating with others to create meaningful applications.
+              My name is Kenneth Achumahara, a Software Developer. I earned my
+              first degree in Accounting and most recently obtained a
+              Certificate in Full Stack Web Development from the University of
+              Arizona. With a disciplined mindset, I love taking difficult tasks
+              and rendering valued solutions that are exceptional with unique
+              qualities. Enthusiastic about approaching programming challenges
+              from different angles and collaborating with others to create
+              meaningful applications.
             </p>
           </div>
-          <iframe frameborder="0" scrolling="no" src="https://arizona.credly.com/embed.html#/?member_badge_id=23348148" width="180" height="230"></iframe>
+          <iframe
+            frameborder="0"
+            scrolling="no"
+            src="https://arizona.credly.com/embed.html#/?member_badge_id=23348148"
+            width="180"
+            height="230"
+          ></iframe>
         </div>
       </Row>
       <Row>
-          <Banner/>
+        <Banner />
       </Row>
     </Container>
   );
---
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import portfoliPicOne from '../images/11.jpg';
+import portfolioPicTwo from '../images/3.jpg';
+import portfolioPicThree from '../images/12.jpg';
+import portfolioPicFour from '../images/5.jpg';
+import portfolioPicFive from '../images/6.jpg';
+import portfolioPicSix from '../images/7.jpg';
+import portfolioPicSeven from '../images/9.jpg';
+import portfolioPicEight from '../images/13.jpg';
+import portfolioPicNine from '../images/4.jpg';
+import portfolioPicTen from '../images/2.jpg';
+import portfolioPicEleven from '../images/8.gif';
+import portfolioPicTwelve from '../images/16.jpg';
+import portfolioPicThirteen from '../images/15.jpg';
+import PortfolioItem from './PortfolioItem.jsx';
+import Container from '../Container/Container.jsx';
+import './Portfolio.css';
+
+const data = [
+  {
+    // id: ,
+    // title: ""
+    // bootcamp: true,
+    // independent: false,
+    // favorite: true,
+    // image: 
+    // url:
+    // overview:
+  }
+]
